2605681db98bceddd0afc4c629ed5e3add88b710
KhadimulRoni <mdkhadimul7066@gmail.com>
2021-11-15 22:44:20 +0600

    delete added

--- src/pages/Home/Orders/Orders/Orders.js
+++ src/pages/Home/Orders/Orders/Orders.js
@@ -16,6 +16,15 @@ const Orders = () => {
       return <Spinner className="m-5" animation="border" variant="warning" />;
    }
 
+   const handleDelete = id => {
+      fetch(`http://localhost:5000/deleteOrder/${id}`, {
+         method: 'DELETE',
+         headers: { 'Content-Type': 'application/json' },
+      })
+         .then(res => res.json())
+         .then(data => console.log(data));
+   };
+
    return (
       <div style={{ textAlign: 'center' }}>
          <h2>MY ORDERS</h2>
@@ -31,7 +40,7 @@ const Orders = () => {
                   className="col-lg-12 col-sm-12 py-2 my-2 d-flex"
                   key={order._id}
                >
-                  <div className="px-5 col-lg-3 col-sm-12">
+                  <div className="px-5 col-lg-2 col-sm-12">
                      <img
                         style={{ width: '150px' }}
                         src={order?.image}
@@ -40,13 +49,19 @@ const Orders = () => {
                   </div>
                   <div
                      style={{ textAlign: 'left' }}
-                     className="col-lg-9 col-sm-12"
+                     className="col-lg-10 col-sm-12"
                   >
                      <h5>{order?.name}</h5>
                      <h4>Price: {order?.price}$</h4>
                      <h6>Number: {order?.number}</h6>
                      <h6>Comments: {order?.comments}</h6>
                      <h6>Date: {order?.date}</h6>
+                     <button
+                        onClick={() => handleDelete(order?._id)}
+                        className="btn-danger"
+                     >
+                        DELETE
+                     </button>
                   </div>
                </div>
             ))}
